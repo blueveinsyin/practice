a399374882fe5861d550cfb5a37eff985eed40df
zq <zhaoqin_yin@163.com>
2021-08-09 16:52:24 +0800

    更新代码，使手写jquery具有原型链

--- jquery封装/dist/index.61840000.js
+++ jquery封装/dist/index.61840000.js
@@ -457,6 +457,11 @@ function hmrAcceptRun(bundle, id) {
 },{}]},["7nfTv"], null, "parcelRequire94c2")
 // jquery是一个不需要加new的构造函数
 // jquery对象指的是jquery函数构造出来的对象
-jQuery('.test').find.call(jQuery('.test'), '.child').parent().print();
+jQuery('.test').find.call(jQuery('.test'), '.child').parent.call(jQuery('.test')).print.call(jQuery('.test'));
+// 类似于bash alias
+window.$ = window.jQuery // 命名风格的问题
+ // 如果是普通的div元素 用el开头命名
+ // 如果是jQuery的元素 用$开头命名
+;
 
 //# sourceMappingURL=index.61840000.js.map
--- jquery封装/src/main.js
+++ jquery封装/src/main.js
@@ -1,6 +1,6 @@
 // jquery是一个不需要加new的构造函数
 // jquery对象指的是jquery函数构造出来的对象
-jQuery('.test').find.call(jQuery('.test'),'.child').parent().print()
+jQuery('.test').find.call(jQuery('.test'),'.child').parent.call(jQuery('.test')).print.call(jQuery('.test'))
 // 类似于bash alias
 window.$ = window.jQuery
 
