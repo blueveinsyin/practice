99aba442d9b697258c34e1a47648ac3d0f902516
zq <zhaoqin_yin@163.com>
2021-08-09 14:45:26 +0800

    添加了一点关于jquery的笔记

--- jquery封装/src/main.js
+++ jquery封装/src/main.js
@@ -1,5 +1,9 @@
 // jquery是一个不需要加new的构造函数
 // jquery对象指的是jquery函数构造出来的对象
 jQuery('.test').find.call(jQuery('.test'),'.child').parent().print()
+// 类似于bash alias
+window.$ = window.jQuery
 
-
+// 命名风格的问题
+// 如果是普通的div元素 用el开头命名
+// 如果是jQuery的元素 用$开头命名
